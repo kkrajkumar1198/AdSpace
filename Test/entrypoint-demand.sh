a449833c7deb46a7101c7d1b823769b13eedec54
Rajkumar_083 <rk@Rajkumars-MacBook-Air.local>
2023-07-24 18:54:16 +0530

    Restructed the files

--- Test/entrypoint-demand.sh
+++
@@ -1,5 +0,0 @@
-
-# wait-for "${DATABASE_HOST}:${DATABASE_PORT}" -- "$@"
-# Watch your .go files and invoke go build if the files changed.
-
-CompileDaemon --build="go build -o DemandSideService/main DemandSideService/main.go"  --command=DemandSideService/main
